69b04bb4ebdd92eae489a75e783a04bdffb23d85
eliampimentel <eliampimentel@prodominicana.gob.do>
2023-09-05 12:51:28 -0400

    some changes

--- src/data/data.controller.ts
+++ src/data/data.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Controller, Get, Param, Res, Post, Body } from '@nestjs/common';
 
 const fs = require('fs');
 const path = require('path');
@@ -10,5 +10,20 @@ getImage(@Param('imageName') imageName: string, @Res() res): void {
   const imagePath = path.join(__dirname, '../../../public/data/images', imageName);
   const fileStream = fs.createReadStream(imagePath);
   fileStream.pipe(res);
+}
+
+@Post('image')
+uploadImage(@Res() res, @Body() body): void {
+    const base64Data = body.base64.replace(/^data:image\/([\w+/]+);base64,/, '');
+    const fileExtension = body.base64.substring("data:image/".length, body.base64.indexOf(";base64"));
+    const imageName = `${new Date().getTime()}.${fileExtension}`;
+    fs.writeFile(path.join(__dirname, '../../../../public/data/images', imageName), base64Data, 'base64', (err) => {
+        if (err) {
+            res.status(500).json({ error: err });
+        } else {
+            res.status(200).json({ imageName });
+        }
+    });
+    
 }
 }
