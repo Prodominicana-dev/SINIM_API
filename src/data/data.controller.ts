a1e82b3f7f4220b73d0b021580f8f1fed37b250f
eliampimentel <eliampimentel@prodominicana.gob.do>
2023-09-06 11:22:09 -0400

    src image change

--- src/data/data.controller.ts
+++ src/data/data.controller.ts
@@ -7,7 +7,7 @@ export class DataController {
 
 @Get('image/:imageName')
 getImage(@Param('imageName') imageName: string, @Res() res): void {
-  const imagePath = path.join(__dirname, '../../../public/data/images', imageName);
+  const imagePath = path.join(__dirname, '../../../../public/data/images', imageName);
   const fileStream = fs.createReadStream(imagePath);
   fileStream.pipe(res);
 }
@@ -17,7 +17,7 @@ uploadImage(@Res() res, @Body() body): void {
     const base64Data = body.base64.replace(/^data:image\/([\w+/]+);base64,/, '');
     const fileExtension = body.base64.substring("data:image/".length, body.base64.indexOf(";base64"));
     const imageName = `${new Date().getTime()}.${fileExtension}`;
-    fs.writeFile(path.join(__dirname, '../../../../../public/data/images', imageName), base64Data, 'base64', (err) => {
+    fs.writeFile(path.join(__dirname, '../../../../public/data/images', imageName), base64Data, 'base64', (err) => {
         if (err) {
             res.status(500).json({ error: err });
         } else {
