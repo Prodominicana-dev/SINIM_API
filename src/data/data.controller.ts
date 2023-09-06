97baea4ae01ae5b0196c239366e9febf6c7a62a1
Eliam Pimentel <eliampimentel@prodominicana.gob.do>
2023-09-06 11:49:51 -0400

    Add '/public' to '/dist' when i build the project

--- src/data/data.controller.ts
+++ src/data/data.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param, Res, Post, Body } from '@nestjs/common';
+import { Controller, Get, Param, Res, Post, Body, StreamableFile } from '@nestjs/common';
+import { createReadStream } from 'fs';
 
 const fs = require('fs');
 const path = require('path');
@@ -7,7 +8,7 @@ export class DataController {
 
 @Get('image/:imageName')
 getImage(@Param('imageName') imageName: string, @Res() res): void {
-  const imagePath = path.join(__dirname, '../../../../public/data/images', imageName);
+  const imagePath = path.join(__dirname, '../../public/data/images', imageName);
   const fileStream = fs.createReadStream(imagePath);
   fileStream.pipe(res);
 }
@@ -17,7 +18,7 @@ uploadImage(@Res() res, @Body() body): void {
     const base64Data = body.base64.replace(/^data:image\/([\w+/]+);base64,/, '');
     const fileExtension = body.base64.substring("data:image/".length, body.base64.indexOf(";base64"));
     const imageName = `${new Date().getTime()}.${fileExtension}`;
-    fs.writeFile(path.join(__dirname, '../../../../public/data/images', imageName), base64Data, 'base64', (err) => {
+    fs.writeFile(path.join(__dirname, '../../public/data/images', imageName), base64Data, 'base64', (err) => {
         if (err) {
             res.status(500).json({ error: err });
         } else {
@@ -25,5 +26,11 @@ uploadImage(@Res() res, @Body() body): void {
         }
     });
     
+}
+
+@Get('file/:fileName')
+getFile(@Param('fileName') fileName: string, @Res() res): StreamableFile {
+    const file = createReadStream(path.join(__dirname, '/public/data/images/', fileName));
+    return new StreamableFile(file);
 }
 }
