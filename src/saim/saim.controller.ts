50ca26b7a936f3eb99a4f2b98101ce51d24cfc22
Eliam Pimentel <139479508+eliampimentel@users.noreply.github.com>
2023-09-21 14:42:51 -0400

    bug edit saim fixed

--- src/saim/saim.controller.ts
+++ src/saim/saim.controller.ts
@@ -33,35 +33,39 @@ export class SaimController {
     async updateSAIM(@Param('id') id: string, @Body() data, @UploadedFile() file: Express.Multer.File, @Res() res) {
         const saim = await this.saimService.getSAIMById(Number(id));
         // Convertir data.products y data.countries a JSON
-        if(data.products) {
+        if (data.products) {
             data.products = JSON.parse(data.products);
         }
-        if(data.countries) {
+        if (data.countries) {
             data.countries = JSON.parse(data.countries);
         }
-        console.log(data)
         // Si el file no es undefined, actualizar la imagen, borrar la foto de esa carpeta y agregar la nueva
         if (file) {
             const folderPath = path.join(process.cwd(), `public/data/saim/images/${id}`);
+            if (fs.existsSync(folderPath)) {
+                await fs.promises.rm(folderPath, { recursive: true }); // Utilizar fs.promises.rmdir para eliminar el directorio de forma asincrónica
+            }
+            await mkdirp(folderPath);
             const imageName = `${new Date().getTime()}.${file.originalname.split('.').pop()}`;
-            fs.writeFile(path.join(folderPath, imageName), file.buffer, (err) => {
+            fs.writeFile(path.join(folderPath, imageName), file.buffer, async (err) => {
                 if (err) {
-                    res.status(500).json({ error: err });
-                } else {
-                    saim.image = imageName;
-                    this.saimService.updateSAIM(Number(id), data);
-                    res.status(200).json({ message: data });
+                    return res.status(500).json({ error: err });
                 }
+                data.image = imageName;
+                await this.saimService.updateSAIM(Number(id), data);
+                res.status(200).json({ message: data });
+            });
+        } else {
+            // Si no se subió un archivo, simplemente actualiza los datos
+            await this.saimService.updateSAIM(Number(id), data).then((saim) => {
+                if (res.statusCode === 500) {
+                    return res.status(500).json({ message: 'Error' });
+                }
+                return res.status(200).json({ message: saim });
             });
         }
-        await this.saimService.updateSAIM(Number(id), data).then((saim) => {
-            if(res.statusCode === 500) {
-                return res.status(500).json({ message: 'Error' });
-            }
-            return res.status(200).json({ message: saim });
-        });
-        
     }
+
 
     @Post()
     @UseInterceptors(FileInterceptor('file'))
