87a1d746903539c8addf293acf355d27cfbb0613
eliampimentel <eliampimentel@prodominicana.gob.do>
2023-10-16 15:00:16 -0400

    seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -24,214 +24,214 @@ const countryURL = 'http://127.0.0.1:3001/apiv2/countries';
 const ramisURL = 'http://127.0.0.1:3001/apiv2/rami';
 
 async function seedDatabase() {
-  // Dominios reservados
+ // Dominios reservados
 
-//   for (const domain of domains) {
-//     await prisma.reservedDomains.create({
-//       data: {
-//         name: domain.name,
-//         platform: domain.platform,
-//       },
-//     });
-//   }
+  for (const domain of domains) {
+    await prisma.reservedDomains.create({
+      data: {
+        name: domain.name,
+        platform: domain.platform,
+      },
+    });
+  }
 
-//   // Crear productos
-//   for(const product of products){
+  // Crear productos
+  for(const product of products){
     
-//     await prisma.product.create({
-//       data: {
-//         name: product.Producto,
-//         code: product.SubPartida,
-//         oldID: product.Id 
-//       }
-//     })
+    await prisma.product.create({
+      data: {
+        name: product.Producto,
+        code: product.SubPartida,
+        oldID: product.Id 
+      }
+    })
         
-//   }
+  }
 
-// for(const country of countries){
-//   await prisma.country.create({
-//     data: {
-//       name: country.name,
-//       abbreviation: country.abbreviation,
-//       continent: country.continent,
-//       group: country.group
-//     },
-//   });
-// }
+for(const country of countries){
+  await prisma.country.create({
+    data: {
+      name: country.name,
+      abbreviation: country.abbreviation,
+      continent: country.continent,
+      group: country.group
+    },
+  });
+}
 
-// // Acuerdo comercial en rami
-// let productsRami;
-// await axios.get(productURL).then((response) => {
-//   productsRami = response.data;
-// });
+// Acuerdo comercial en rami
+let productsRami;
+await axios.get(productURL).then((response) => {
+  productsRami = response.data;
+});
 
-// const getProductId = async (id) => {
-//   for(const product of productsRami){
-//     if(product.oldID == id){
-//       return product.id;
-//     }
-//   }
-// }
+const getProductId = async (id) => {
+  for(const product of productsRami){
+    if(product.oldID == id){
+      return product.id;
+    }
+  }
+}
 
 
-// for(const trade of tradeAgreement){
-//   const productID = await getProductId(trade.Id_Producto);
-//   await prisma.ramis.create({
-//     data: {
-//       productId: productID,
-//       countryId: trade.IdPais,
-//       tradeAgreement: trade.AcuerdoComercial,
-//     }
-//   })
-// }
+for(const trade of tradeAgreement){
+  const productID = await getProductId(trade.Id_Producto);
+  await prisma.ramis.create({
+    data: {
+      productId: productID,
+      countryId: trade.IdPais,
+      tradeAgreement: trade.AcuerdoComercial,
+    }
+  })
+}
 
-// let ramis;
-// await axios.get(ramisURL).then((response) => {
-//   ramis = response.data;
-// });
-// const getRAMIId = async (pid, cid) => {
-//   for(const rami of ramis){
-//     if(rami.productId == pid && rami.countryId == cid){
-//       return rami.id;
-//     }
-//   }
-// }
+let ramis;
+await axios.get(ramisURL).then((response) => {
+  ramis = response.data;
+});
+const getRAMIId = async (pid, cid) => {
+  for(const rami of ramis){
+    if(rami.productId == pid && rami.countryId == cid){
+      return rami.id;
+    }
+  }
+}
 
-// // Agregar Aranceles Impuestos a cada RAMI
-// for(const tariff of tariffs){
-//   const productId = await getProductId(tariff.Id_Producto);
-//   const ramiID = await getRAMIId(productId, tariff.IdPais);
-//   if(ramiID == undefined){
-//     continue;
-//   }
-//   await prisma.ramis.update({
-//     where: {
-//       id: ramiID
-//     },
-//     data: {
-//       tariffsImposed: tariff.ArancelesImpuesto,
-//     }
-//   })
-// }
+// Agregar Aranceles Impuestos a cada RAMI
+for(const tariff of tariffs){
+  const productId = await getProductId(tariff.Id_Producto);
+  const ramiID = await getRAMIId(productId, tariff.IdPais);
+  if(ramiID == undefined){
+    continue;
+  }
+  await prisma.ramis.update({
+    where: {
+      id: ramiID
+    },
+    data: {
+      tariffsImposed: tariff.ArancelesImpuesto,
+    }
+  })
+}
 
-// // Agregar Recursos Web a cada RAMI
-// for(const web of webResource){
-//   const productId = await getProductId(web.Id_Producto);
-//   const ramiID = await getRAMIId(productId, web.IdPais);
-//   if(ramiID == undefined){
-//     continue;
-//   }
-//   await prisma.ramis.update({
-//     where: {
-//       id: ramiID
-//     },
-//     data: {
-//       webResource: web.Recurso,
-//     }
-//   })
-// }
+// Agregar Recursos Web a cada RAMI
+for(const web of webResource){
+  const productId = await getProductId(web.Id_Producto);
+  const ramiID = await getRAMIId(productId, web.IdPais);
+  if(ramiID == undefined){
+    continue;
+  }
+  await prisma.ramis.update({
+    where: {
+      id: ramiID
+    },
+    data: {
+      webResource: web.Recurso,
+    }
+  })
+}
 
-// // Agregar Regulaciones Tecnicas a cada RAMI
-// for(const tech of technicalRequirements){
-//   const productId = await getProductId(tech.Id_Producto);
-//   const ramiID = await getRAMIId(productId, tech.IdPais);
-//   if(ramiID == undefined){
-//     continue;
-//   }
-//   await prisma.ramis.update({
-//     where: {
-//       id: ramiID
-//     },
-//     data: {
-//       technicalRequirements: tech.RequisitosTecnicos,
-//       permitsCertifications: tech.PermisosCertificaciones,
-//       labelingCertifications: tech.EtiquetadoCertificado
-//     }
-//   })
-// }
+// Agregar Regulaciones Tecnicas a cada RAMI
+for(const tech of technicalRequirements){
+  const productId = await getProductId(tech.Id_Producto);
+  const ramiID = await getRAMIId(productId, tech.IdPais);
+  if(ramiID == undefined){
+    continue;
+  }
+  await prisma.ramis.update({
+    where: {
+      id: ramiID
+    },
+    data: {
+      technicalRequirements: tech.RequisitosTecnicos,
+      permitsCertifications: tech.PermisosCertificaciones,
+      labelingCertifications: tech.EtiquetadoCertificado
+    }
+  })
+}
 
-// // Agregar Requisitos de Salida a cada RAMI
-// for(const output of outputRequirement){
-//   const productId = await getProductId(output.Id_Producto);
-//   const ramiID = await getRAMIId(productId, output.IdPais);
-//   if(ramiID == undefined){
-//     continue;
-//   }
-//   await prisma.ramis.update({
-//     where: {
-//       id: ramiID
-//     },
-//     data: {
-//       outputRequirement: output.ResquisitoSalida
-//     }
-//   })
-// }
+// Agregar Requisitos de Salida a cada RAMI
+for(const output of outputRequirement){
+  const productId = await getProductId(output.Id_Producto);
+  const ramiID = await getRAMIId(productId, output.IdPais);
+  if(ramiID == undefined){
+    continue;
+  }
+  await prisma.ramis.update({
+    where: {
+      id: ramiID
+    },
+    data: {
+      outputRequirement: output.ResquisitoSalida
+    }
+  })
+}
 
-// // Agregar Requisitos de Importacion a cada RAMI
-// for(const input of importRequirement){
-//   const productId = await getProductId(input.Id_Producto);
-//   const ramiID = await getRAMIId(productId, input.IdPais);
-//   if(ramiID == undefined){
-//     continue;
-//   }
-//   await prisma.ramis.update({
-//     where: {
-//       id: ramiID
-//     },
-//     data: {
-//       importRequirement: input.RequisitoImportacion
-//     }
-//   })
-// }
+// Agregar Requisitos de Importacion a cada RAMI
+for(const input of importRequirement){
+  const productId = await getProductId(input.Id_Producto);
+  const ramiID = await getRAMIId(productId, input.IdPais);
+  if(ramiID == undefined){
+    continue;
+  }
+  await prisma.ramis.update({
+    where: {
+      id: ramiID
+    },
+    data: {
+      importRequirement: input.RequisitoImportacion
+    }
+  })
+}
 
-// // SAIM
-// // Agregar SAIM's
-// for(const s of saims){
-//   // Separar productos para crear un JSON con todos los productos y codigo arancelario de c/u
-//   const products = s.Productos.split(',');
-//   const productsJSON = [];
-//   for(const product of products){
-//     const productID = await prisma.product.findFirst({
-//       where: {
-//         name: product
-//       }
-//     })
-//     if(productID == null){
-//       continue;
-//     }
-//     productsJSON.push({
-//       ...productID
-//     })
-//   }
-//   // Separar paises y crear JSON con todos sus datos
-//   const countries = s.Pais.split(',');
-//   const countriesJSON = [];
-//   for(const country of countries){
-//     const countryID = await prisma.country.findFirst({
-//       where: {
-//         name: country
-//       }
-//     })
-//     if(countryID == null){
-//       continue;
-//     }
-//     countriesJSON.push({
-//       ...countryID
-//     })
-//   }
-//   await prisma.alerts.create({
-//     data: {
-//       title: s.Titular,
-//       description: s.Contenido,
-//       category: s.Clasificacion,
-//       image: s.Imagen,
-//       products: productsJSON,
-//       countries: countriesJSON,
-//       platform: "saim",
-//       oldID: s.Id
-//     }
-//   })
-// }
+// SAIM
+// Agregar SAIM's
+for(const s of saims){
+  // Separar productos para crear un JSON con todos los productos y codigo arancelario de c/u
+  const products = s.Productos.split(',');
+  const productsJSON = [];
+  for(const product of products){
+    const productID = await prisma.product.findFirst({
+      where: {
+        name: product
+      }
+    })
+    if(productID == null){
+      continue;
+    }
+    productsJSON.push({
+      ...productID
+    })
+  }
+  // Separar paises y crear JSON con todos sus datos
+  const countries = s.Pais.split(',');
+  const countriesJSON = [];
+  for(const country of countries){
+    const countryID = await prisma.country.findFirst({
+      where: {
+        name: country
+      }
+    })
+    if(countryID == null){
+      continue;
+    }
+    countriesJSON.push({
+      ...countryID
+    })
+  }
+  await prisma.alerts.create({
+    data: {
+      title: s.Titular,
+      description: s.Contenido,
+      category: s.Clasificacion,
+      image: s.Imagen,
+      products: productsJSON,
+      countries: countriesJSON,
+      platform: "saim",
+      oldID: s.Id
+    }
+  })
+}
 
 // Datamarket
 
