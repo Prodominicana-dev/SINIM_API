ab62042c401ffc29952cf23e3ece7582595505dc
eliampimentel <eliampimentel@prodominicana.gob.do>
2023-11-20 11:11:47 -0400

    Product update

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,49 +1,58 @@
-import { Controller, Get, Post, Body, Param, ValidationPipe, Patch, Delete, Put } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  ValidationPipe,
+  Patch,
+  Delete,
+  Put,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 
 @Controller('apiv2/')
 export class ProductController {
-    constructor(private productService : ProductService) {}
+  constructor(private productService: ProductService) {}
 
-    // Product list
-    @Get('products')
-    async getProducts(){
-        return this.productService.getActiveProducts();
-    }
+  // Product list
+  @Get('products')
+  async getProducts() {
+    return this.productService.getActiveProducts();
+  }
 
-    @Get('products/all')
-    async getAllProducts(){
-        return this.productService.getProducts();
-    }
+  @Get('products/all')
+  async getAllProducts() {
+    return this.productService.getProducts();
+  }
 
-    // Find product by id
-    @Get('product/:id')
-    async getProductById(id: number){
-        return this.productService.findOne(Number(id));
-    }
+  // Find product by id
+  @Get('product/:id')
+  async getProductById(id: number) {
+    return this.productService.findOne(Number(id));
+  }
 
-    //Get products with label and value
-    @Get('products/select')
-    async getProductsLabelValue(){
-        return this.productService.getProductsLabelValue();
-    }
+  //Get products with label and value
+  @Get('products/select')
+  async getProductsLabelValue() {
+    return this.productService.getProductsLabelValue();
+  }
 
-    // Create product
-    @Post('product')
-    async createProduct(@Body() data: any){
-        return this.productService.create(data).then((res) => {
-        });
-    }
+  // Create product
+  @Post('product')
+  async createProduct(@Body() data: any) {
+    return this.productService.create(data).then((res) => {});
+  }
 
-    // Edit product
-    @Put('product/:id')
-    async editProduct(@Param('id') id: number, @Body() data: any){
-        return this.productService.edit(Number(id), data);
-    }
+  // Edit product
+  @Put('product/:id')
+  async editProduct(@Param('id') id: number, @Body() data: any) {
+    return this.productService.edit(Number(id), data);
+  }
 
-    // Delete product
-    @Delete('product/:id')
-    async deleteProduct(@Param('id') id: number){
-        return this.productService.deactivate(Number(id));
-    }
+  // Delete product
+  @Patch('product/:id')
+  async deleteProduct(@Param('id') id: number) {
+    return this.productService.deactivate(Number(id));
+  }
 }
